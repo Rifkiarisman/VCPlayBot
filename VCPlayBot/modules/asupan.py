0ad190384ccda4567f872376a8b2f4e9804cd222
KyyProject <83812990+Rifkiarisman@users.noreply.github.com>
2021-08-28 00:43:26 +0700

    Create asupan.py

---
+++ VCPlayBot/modules/asupan.py
@@ -0,0 +1,33 @@
+import requests
+from pyrogram import Client
+from config import BOT_USERNAME
+from helpers.filters import command
+
+@Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
+async def asupan(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Sabar ya sayang nanti dikasi asupan`")
+
+
+@Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
+async def wibu(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Run,Ada wibu`")
+
+
+@Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
+async def chika(client, message):
+    try:
+        resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results)
+    except Exception:
+        await message.reply_text("`Chika nya Istirahat `")
